0ee476ccb80d9fac23eecf6f84380b2820cf9797
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-02-22 18:12:50 +0900

    it works

--- run.py
+++ run.py
@@ -10,14 +10,46 @@ def main():
     conn = create_db_connection(args.host, args.user, args.passwd, db)
 
     dic = {}
+    samples = 2
 
-    for i in range(1):
+    processes = {}
+
+    for i in range(args.ts):
+
+        for d in dic:
+            for j in range(samples-1, 0, -1):
+                dic[d][j] = dic[d][j-1]
+
         for p in get_process_list(conn):
-            print(p)
+            ID = p.get("ID")
+            if dic.get(ID, None) is None:
+                dic[ID] = [0]*samples
+                processes[ID] = p
 
-        sleep(1)
+            dic[ID][0] += args.delimiter
 
-    print(sorted(dic))
+        # count = 0
+        # for d in sorted(dic.items(), key=lambda x: x[1][0], reverse=True):
+        #     if count > 5:
+        #         break
+        #     print(d)
+        #     count += 1
+
+        print("-----------", i, "-----------")
+        del_list = []
+        for d in dic:
+            if dic[d][0] == dic[d][1]:
+                print(d, dic[d][0], processes[d].get("HOST"), processes[d].get("PORT"))
+
+            if dic[d][0] == dic[d][samples-1]:
+                del_list.append(d)
+
+        for d in del_list:
+            dic.pop(d)
+            processes.pop(d)
+
+        sleep(args.delimiter)
+
     conn.close
 
 
@@ -73,7 +105,9 @@ def define_parsers():
                         help='mysql host')
     parser.add_argument('-p', '--passwd', type=str, default="",
                         help='mysql password')
-    parser.add_argument('-d', type=str, default="1",
+    parser.add_argument('-d', "--delimiter", type=int, default="1",
+                        help='sleep range')
+    parser.add_argument('-t', "--ts", type=int, default="1",
                         help='sleep range')
 
     return parser.parse_args()
